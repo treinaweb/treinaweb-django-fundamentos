5c8d4db8d4da4d41c39335195f092f3ae21a88a8
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-11-22 11:06:06 -0300

    04.01 - Configuração do MySQL

---
+++ tw_clientes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tw_clientes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tw_clientes.settings')
+
+application = get_wsgi_application()
