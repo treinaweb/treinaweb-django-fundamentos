1fa6246f5a6523c6749bed6e05d8985f2429dd46
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-11-23 11:42:17 -0300

    06.03 - Criando a primeira view

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'clientes/lista_clientes.html', {'clientes': clientes})
