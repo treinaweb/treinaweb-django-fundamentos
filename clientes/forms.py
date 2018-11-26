2adca644c2e7f9c3c82b2aea02b4102d62ca4d79
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-11-26 14:40:48 -0300

    09.03 - Criando campo csrf_token

--- clientes/forms.py
+++ clientes/forms.py
@@ -3,7 +3,6 @@ from .models import Cliente
 
 
 class ClienteForm(forms.ModelForm):
-    nome = forms.CharField(widget=forms.Textarea())
     class Meta:
         model = Cliente
         fields = ['nome', 'sexo', 'data_nascimento', 'email', 'profissao']
