97d803a00696ebcb1f62d096317d70c26da5c003
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-11-26 10:49:59 -0300

    09.01 - Criando form de clientes

---
+++ clientes/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Cliente
+
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'sexo', 'data_nascimento', 'email', 'profisao']
