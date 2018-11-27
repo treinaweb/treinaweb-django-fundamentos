35b48850e015741f602ce209d7bd072aec6900e4
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2018-11-27 14:42:45 -0300

    10.02 - Extendendo templates

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cliente
 from .forms import ClienteForm
 
@@ -13,6 +13,7 @@ def inserir_cliente(request):
         form = ClienteForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('listar_clientes')
     else:
         form = ClienteForm()
     return render(request, 'clientes/form_cliente.html', {'form': form})
